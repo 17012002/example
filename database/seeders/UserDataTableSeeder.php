f283a6e14e879a84fb4b6043fab373d4ac8f7179
lorenzo <lorenzo.labarta.arilla@gmail.com>
2023-03-04 02:37:48 +0000

    Seeder set up

--- database/seeders/UserDataTableSeeder.php
+++ database/seeders/UserDataTableSeeder.php
@@ -18,5 +18,29 @@ class UserDataTableSeeder extends Seeder
         $user1->password="contraseña21";
         $user1->gender="M";
         $user1->save();
+
+        $user2 = new UserData;
+        $user2->name="Loren";
+        $user2->password="contraseña22";
+        $user2->gender="M";
+        $user2->save();
+
+        $user3 = new UserData;
+        $user3->name="Loren";
+        $user3->password="contraseña23";
+        $user3->gender="M";
+        $user3->save();
+
+        $user4 = new UserData;
+        $user4->name="Loren";
+        $user4->password="contraseña24";
+        $user4->gender="M";
+        $user4->save();
+
+        $user5 = new UserData;
+        $user5->name="Loren";
+        $user5->password="contraseña25";
+        $user5->gender="M";
+        $user5->save();
     }
 }
