a12f704354481559911c42938319f91457766acd
lorenzo <lorenzo.labarta.arilla@gmail.com>
2023-05-11 21:35:24 +0100

    communities error

--- database/seeders/SubCommunityTableSeeder.php
+++ database/seeders/SubCommunityTableSeeder.php
@@ -19,6 +19,6 @@ class SubCommunityTableSeeder extends Seeder
         $i->people_with_common_interests=132;
         $i->save();
         
-        SubCommunity::factory()->count(4)->create();
+        SubCommunity::factory()->count(10)->create();
     }
 }
