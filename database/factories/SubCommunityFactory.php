5a97501c05c1f90a420a8f441a73a34a75fb721e
lorenzo <lorenzo.labarta.arilla@gmail.com>
2023-05-11 21:17:40 +0100

    Communities fixed

--- database/factories/SubCommunityFactory.php
+++ database/factories/SubCommunityFactory.php
@@ -15,9 +15,13 @@ class SubCommunityFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-    {        
+    {
+        $topics = ['Technology', 'Art', 'Sports', 'Health', 'Science', 'Travel', 'Food', 'Fashion', 'Education', 'Music', 'Film', 'Business', 'Politics', 'Environment', 'Culture', 'History', 'Social', 'Entertainment', 'Media', 'Design', 'Gaming', 'Literature', 'Beauty', 'Fitness', 'Photography', 'Comedy', 'Parenting', 'Pets', 'Spirituality', 'Relationships', 'Self-Improvement', 'DIY', 'Crafts', 'Cars', 'Finance', 'Real Estate', 'Home', 'Outdoors', 'Gardening', 'Charity', 'Volunteering', 'Adventure', 'Investing', 'Marketing', 'Law', 'Education', 'Fashion', 'Fitness', 'Travel', 'Business'];
+        
+        $name = $this->faker->unique()->randomElements($topics, 10);
+        
         return [
-            'name' => $this->faker->country(),
+            'name' => implode(', ', $name),
             'members' => $this->faker->numberBetween(1, 10000),
         ];
     }
